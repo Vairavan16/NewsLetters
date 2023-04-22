3b0dc2e770f4de2444538a57da564afbaa7ef2cc
Vairavan16 <kannangold16@gmail.com>
2023-04-22 17:40:52 +0530

    alaginment of code commit 5

--- app.js
+++ app.js
@@ -8,62 +8,62 @@ const port = 3000 || process.env.PORT;
 
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-      app.get('/', (req, res) => {
-        res.sendFile(__dirname + "/signup.html")
-        })
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/signup.html")
+})
 
-        app.post('/',(req, res) => {
-            var fname =req.body.firstName;
-            var lname =req.body.lastName;
-            var mail = req.body.email;
-           
-            console.log(fname)
-            console.log(lname)
-            console.log(mail)
+app.post('/', (req, res) => {
+  var fname = req.body.firstName;
+  var lname = req.body.lastName;
+  var mail = req.body.email;
 
-            mailchimp.setConfig({
-              apiKey: "54db10a420eada3b89436225007e916f-us21",
-              server: "us21"
-            });
-            
-            const subscribingUser = {
-                firstName:fname,
-                lastName: lname,
-                email: mail
-              };
-            async function run() {
+  console.log(fname)
+  console.log(lname)
+  console.log(mail)
 
-              const response = await mailchimp.lists.addListMember(listId,{
-                email_address: subscribingUser.email,
-                status: "subscribed",
-                merge_fields: {
-                  FNAME: subscribingUser.firstName,
-                  LNAME: subscribingUser.lastName,
-                } 
-              }).then(
-                (value) => {
-                  
-                    console.log("Successfully added contact as an audience member.");
-                    res.sendFile(__dirname + "/success.html");
-                    console.log(value);
-                    console.log("Successfully added as an audience member. The contact's id is "+value.id);
-                },
-                (reason) => {
-                  res.sendFile(__dirname + "/failure.html");
-                    console.log(reason);
-                },
-            );    
-        };
-      run();
-    });
+  mailchimp.setConfig({
+    apiKey: "54db10a420eada3b89436225007e916f-us21",
+    server: "us21"
+  });
 
-    app.post("/failure", function(req, res){
-          res.redirect("/")
-    })
+  const subscribingUser = {
+    firstName: fname,
+    lastName: lname,
+    email: mail
+  };
+  async function run() {
 
-    app.listen(port, function(){
-        console.log("server started at port "+port);
-    })
+    const response = await mailchimp.lists.addListMember(listId, {
+      email_address: subscribingUser.email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: subscribingUser.firstName,
+        LNAME: subscribingUser.lastName,
+      }
+    }).then(
+      (value) => {
+
+        console.log("Successfully added contact as an audience member.");
+        res.sendFile(__dirname + "/success.html");
+        console.log(value);
+        console.log("Successfully added as an audience member. The contact's id is " + value.id);
+      },
+      (reason) => {
+        res.sendFile(__dirname + "/failure.html");
+        console.log(reason);
+      },
+    );
+  };
+  run();
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/")
+})
+
+app.listen(port, function () {
+  console.log("server started at port " + port);
+})
 
